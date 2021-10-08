f99b3ac47f8666d374d05919cac9db7558596bf2
Jonathan Gander <jonathan@gander.family>
2021-10-08 17:22:54 +0200

    Display random features is funnier.

--- WhatsNewExample/ViewController.swift
+++ WhatsNewExample/ViewController.swift
@@ -26,12 +26,30 @@ class ViewController: UIViewController, WhatsNewViewControllerDelegate {
     // MARK: -
     func displayWhatsNewViewController() {
         
-        let features = [
-            WhatsNewFeature(title: "First amazing feature", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam accumsan pretium arcu, sit amet porta lectus ultrices sed.", image: UIImage(systemName: "paintbrush")),
-            WhatsNewFeature(title: "Second feature", text: "Sed lacinia tortor nunc, at eleifend mi porta eu.", image: UIImage(systemName: "globe.europe.africa.fill")),
-            WhatsNewFeature(title: "Another one", text: "Sed ex risus, vehicula et finibus et, venenatis vitae nisi.", image: UIImage(systemName: "megaphone")),
+        // *** Set random features (just for fun).
+        var availableFeatures = [
+            WhatsNewFeature(title: "App loads 42x faster", text: "Specially when you refresh your discussions list in the morning.", image: UIImage(systemName: "speedometer")),
+            WhatsNewFeature(title: "Smarter than expected", text: "App can now set an alarm for you according to weather conditions.", image: UIImage(systemName: "alarm")),
+            WhatsNewFeature(title: "Bugs fix", text: "Fix a lot of minor issues. Also add UI improvement in several views.", image: UIImage(systemName: "ant")),
+            WhatsNewFeature(title: "Add your location", text: "Add your location on map and send it to your friends in one tap.", image: UIImage(systemName: "mappin.and.ellipse")),
+            WhatsNewFeature(title: "Sort your files", text: "You can now sort your files by name, size, number of words or date.", image: UIImage(systemName: "arrow.up.arrow.down.square")),
+            WhatsNewFeature(title: "Never miss the train", text: "We will alert you when your train is arriving so you don't have to move your eyes from your phone.", image: UIImage(systemName: "tram")),
+            WhatsNewFeature(title: "More security", text: "All your messages are now signed so your contacts always know it is you.", image: UIImage(systemName: "signature")),
+            WhatsNewFeature(title: "More search results", text: "We have added hotels and B&B in our search results.", image: UIImage(systemName: "bed.double")),
+            WhatsNewFeature(title: "Everybody is a king", text: "Premium account are all deleted. All features are free for everybody!", image: UIImage(systemName: "crown")),
+            WhatsNewFeature(title: "Developers at work", text: "We are still working on reported issues.", image: UIImage(systemName: "screwdriver")),
+            WhatsNewFeature(title: "Want to go hiking?", text: "Download my app GPX viewer from the App Store!", image: UIImage(systemName: "map")),
         ]
         
+        var features = [WhatsNewFeature]()
+        // Add random features
+        for _ in 1...Int.random(in: 2...5) {
+            if let index = availableFeatures.indices.randomElement() {
+                features.append(availableFeatures.remove(at: index))
+            }
+        }
+        
+        // *** Create WhatsNewViewController with selected features and self as delegate
         let vc = WhatsNewViewController .create(withFeatures: features, delegate: self)
         
         // *** Set random parameters (just for fun). Simply change with your own ***
@@ -52,7 +70,7 @@ class ViewController: UIViewController, WhatsNewViewControllerDelegate {
         vc.moreInfoText = ["More info...", "Learn more...", "More about privacy...", "See complete features...", "About..."].randomElement()!
         
         
-        // Display WhatsNewViewController
+        // *** Display WhatsNewViewController
         self.present(vc, animated: true, completion: nil)
     }
     
